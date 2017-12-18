2af7dfe92fe7a90a745e63ff3d6983276bb6b90c
Alexey Vorobiev <alexey@itima.ru>
2017-12-18 18:41:50 +0300

    added backiller

--- backfiller.rb
+++ backfiller.rb
@@ -7,52 +7,72 @@ require 'logger'
 require "timeout"
 
 WORKING_DIRECTORY = Dir.pwd
-API_URL = 'https://poloniex.com/public?command=returnTicker&period=60'
-MIN_VOLUME = 500.0
 
-def get_coin_data
-  response = HTTParty.get(API_URL)
-  coindata = response.parsed_response
-end
+Daemons.run_proc('cointrader_runner.rb') do
+  TIMEOUT = 5*60*60
+  API_URL = 'https://poloniex.com/public?command=returnTicker&period=60'
+  MIN_VOLUME = 500.0
+  ACTION_LOGGER = Logger.new(WORKING_DIRECTORY + '/actions.csv')
 
-def rename_coin(coin, pair = 'BTC')
-  if coin.include?('BTC_')
-    coin_name = coin.split("_").last
-    pair_name = "#{coin_name}-#{pair}"
+  def get_coin_data
+    response = HTTParty.get(API_URL)
+    coindata = response.parsed_response
   end
-end
 
-system "rm ./simulations/*.html"
+  def rename_coin(coin, pair = 'BTC')
+    if coin.include?('BTC_')
+      coin_name = coin.split("_").last
+      pair_name = "#{coin_name}-#{pair}"
+    end
+  end
 
-results = {}
-  
-first_data = get_coin_data
-first_data.select {|coin| coin.include?('BTC_')}.each do |coin, value|
-  puts coin, value
-  puts "value: #{value["baseVolume"]} > 500 #{calc = value["baseVolume"].to_f > MIN_VOLUME} "
-  pair = rename_coin(coin)
-  if calc
-    result = %x[zenbot backfill  poloniex.#{pair} --days 2]
-    puts result
-    result = %x[zenbot sim poloniex.#{pair} --days 1]
-    file = result.split("\n").last.split(" ").last
-    results[pair] = {}
-    buy_hold = false
-    File.open(file).each do |line|
-      if line.include?("end balance")
-        results[pair]["end_balance"] = line.split(" ")[2].to_f
-        results[pair]["end_balance%"] = line.scan(/\(([^)]+)\)/).flatten.first.to_f
-      elsif line.include?("buy hold")
-        if buy_hold == false
-          results[pair]["buy_hold"] = line.split(" ")[2].to_f
-          results[pair]["buy_hold%"] = line.scan(/\(([^)]+)\)/).flatten.first.to_f
-          results[pair]["vs_buy_hold%"] = (1 -  results[pair]["buy_hold"].to_f / results[pair]["end_balance"].to_f)*100.0
-          buy_hold = true
+  def sell(coin)
+    LOGGER.info ">> Selling #{coin}"
+    system "zenbot sell --order_adjust_time 20000  poloniex.#{coin}"
+  end
+
+  loop do
+    system "rm ./simulations/*.html"
+
+    results = {}
+    first_data = get_coin_data
+    first_data.select {|coin| coin.include?('BTC_')}.each do |coin, value|
+      puts coin, value
+      puts "value: #{value["baseVolume"]} > 500 #{calc = value["baseVolume"].to_f > MIN_VOLUME} "
+      pair = rename_coin(coin)
+      if calc
+        result = %x[zenbot backfill  poloniex.#{pair} --days 2]
+        puts result
+        result = %x[zenbot sim poloniex.#{pair} --days 1]
+        file = result.split("\n").last.split(" ").last
+        results[pair] = {}
+        buy_hold = false
+        File.open(file).each do |line|
+          if line.include?("end balance")
+            results[pair]["end_balance"] = line.split(" ")[2].to_f
+            results[pair]["end_balance%"] = line.scan(/\(([^)]+)\)/).flatten.first.to_f
+          elsif line.include?("buy hold")
+            if buy_hold == false
+              results[pair]["buy_hold"] = line.split(" ")[2].to_f
+              results[pair]["buy_hold%"] = line.scan(/\(([^)]+)\)/).flatten.first.to_f
+              results[pair]["vs_buy_hold%"] = (1 -  results[pair]["buy_hold"].to_f / results[pair]["end_balance"].to_f)*100.0
+              buy_hold = true
+            end
+          end
         end
+        puts results[pair]
       end
     end
-    puts results[pair]
+  
+    Timeout.timeout(TIMEOUT) do
+      puts results
+      puts "WINNER: #{winner = results.sort {|a,b| b.last["end_balance"].to_f <=> a.last["end_balance"].to_f}.first}"
+      ACTION_LOGGER.debug results.to_s
+      coin = winner.first
+      system("zenbot trade poloniex.#{coin}")
+    rescue Timeout::Error
+      puts ">> Timeout trading #{coin}"
+      sell(coin)
+    end
   end
 end
-
-puts results
