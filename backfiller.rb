64cbd23aaa48b0986b2522ce5e28a82f6459939a
Alexey Vorobiev <alexey@itima.ru>
2017-12-18 17:34:41 +0300

    added backiller

--- backfiller.rb
+++ backfiller.rb
@@ -7,20 +7,8 @@ require 'logger'
 require "timeout"
 
 WORKING_DIRECTORY = Dir.pwd
-
-
-  API_URL = 'https://poloniex.com/public?command=returnTicker&period=60'
-  TIMEOUT = 50
-  ACTION_TIMEOUT = {"buy" => 10, "sell" =>  150}
-  MIN_VOLUME = 100
-  LOGGER = Logger.new(WORKING_DIRECTORY + '/results.log')
-  ACTION_LOGGER = Logger.new(WORKING_DIRECTORY + '/actions.csv')
-  @old_coin = nil
-  @old_coin_dips = nil
-  @new_coin = nil
-  @action = 'buy'
-  
-  @bad_coins = []#['REP-BTC', 'ZEC-BTC', 'BCN-BTC', 'CLAM-BTC', 'LSK-BTC', 'LBC-BTC', 'ARDR-BTC', 'DOGE-BTC', 'FCT-BTC', 'STEEM-BTC', 'GAME-BTC', 'GNT-BTC']
+API_URL = 'https://poloniex.com/public?command=returnTicker&period=60'
+MIN_VOLUME = 500.0
 
 def get_coin_data
   response = HTTParty.get(API_URL)
@@ -41,7 +29,7 @@ results = {}
 first_data = get_coin_data
 first_data.each do |coin, value|
   puts coin, value
-  puts "value: #{value["baseVolume"]} > 500 #{calc = value["baseVolume"].to_f > 500.0} "
+  puts "value: #{value["baseVolume"]} > 500 #{calc = value["baseVolume"].to_f > MIN_VOLUME} "
   pair = rename_coin(coin)
   if calc
     result = %x[zenbot backfill  poloniex.#{pair} --days 2]
@@ -49,15 +37,21 @@ first_data.each do |coin, value|
     result = %x[zenbot sim poloniex.#{pair} --days 1]
     file = result.split("\n").last.split(" ").last
     results[pair] = {}
+    buy_hold = false
     File.open(file).each do |line|
       if line.include?("end balance")
-        results[pair]["end_balance"] = line.scan(/\(([^)]+)\)/)
+        results[pair]["end_balance"] = line.scan(/\(([^)]+)\)/).flatten
       elsif line.include?("buy hold")
-        results[pair]["buy_hold"] = line.scan(/\(([^)]+)\)/)
+        if buy_hold = false
+          results[pair]["buy_hold"] = line.scan(/\(([^)]+)\)/).flatten
+          buy_hold = true
+        end
       elsif line.include?("vs. buy hold")
-        results[pair][vs_buy_hold] = line.split(" ").last
+        results[pair][vs_buy_hold] = line.split(":").last.strip
       end
     end
     puts results[pair]
   end
 end
+
+puts results
